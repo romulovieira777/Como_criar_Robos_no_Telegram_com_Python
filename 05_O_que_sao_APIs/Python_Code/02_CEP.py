140a7a738ab4c3c65100a3d6e33ac453dff3b01a
Romulo Vieira <romulo.vieira777@gmail.com>
2023-01-26 14:27:20 -0300

    O que são APIs

--- 05_O_que_sao_APIs/Python_Code/02_CEP.py
+++ 05_O_que_sao_APIs/Python_Code/02_CEP.py
@@ -1,123 +1,64 @@
 import requests
 
-
-def get_info_zipcode(zipcode):
+zipcode = input('Digite o CEP: ')
+if zipcode.replace('-', '') and len(zipcode) < 9:
     url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
     resq = requests.get(url_base)
     if resq.status_code == 200:
         d = resq.json()
     else:
-        return "Error"
+        print("Erro")
+else:
+    print("CEP inválido")
 
 
-def get_info_logradouro(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_logradouro():
     return d['logradouro']
 
 
-def get_info_city(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_city():
     return d['localidade']
 
 
-def get_info_uf(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_uf():
     return d['uf']
 
 
-def get_info_ibge(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_ibge():
     return d['ibge']
 
 
-def get_info_gia(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_gia():
     return d['gia']
 
 
-def get_info_ddd(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_ddd():
     return d['ddd']
 
 
-def get_info_siafi(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_siafi():
     return d['siafi']
 
 
-def get_info_complemento(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_complemento():
     return d['complemento']
 
 
-def get_info_bairro(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_bairro():
     return d['bairro']
 
 
-def get_info_cep(zipcode):
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        return "Error"
+def get_info_cep():
     return d['cep']
 
 
-zipcode = input('Digite o CEP: ')
-print(f"CEP: {get_info_cep(zipcode)}")
-print(f"Logradouro: {get_info_logradouro(zipcode)}")
-print(f"Complemento: {get_info_complemento(zipcode)}")
-print(f"Bairro: {get_info_bairro(zipcode)}")
-print(f"Cidade: {get_info_city(zipcode)}")
-print(f"UF: {get_info_uf(zipcode)}")
-print(f"DDD da Cidade: {get_info_ddd(zipcode)}")
-print(f"IBGE: {get_info_ibge(zipcode)}")
-print(f"GIA: {get_info_gia(zipcode)}")
-print(f"Siafi: {get_info_siafi(zipcode)}")
+print(f"CEP: {get_info_cep()}")
+print(f"Logradouro: {get_info_logradouro()}")
+print(f"Complemento: {get_info_complemento()}")
+print(f"Bairro: {get_info_bairro()}")
+print(f"Cidade: {get_info_city()}")
+print(f"UF: {get_info_uf()}")
+print(f"DDD da Cidade: {get_info_ddd()}")
+print(f"IBGE: {get_info_ibge()}")
+print(f"GIA: {get_info_gia()}")
+print(f"Siafi: {get_info_siafi()}")
