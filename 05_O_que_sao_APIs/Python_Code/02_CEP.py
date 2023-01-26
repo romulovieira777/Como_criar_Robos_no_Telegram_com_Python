729ac168f29f2041b15c8d12493f51cc925214ab
Romulo Vieira <romulo.vieira777@gmail.com>
2023-01-26 17:43:21 -0300

    O que são APIs

--- 05_O_que_sao_APIs/Python_Code/02_CEP.py
+++ 05_O_que_sao_APIs/Python_Code/02_CEP.py
@@ -1,16 +1,5 @@
 import requests
 
-zipcode = input('Digite o CEP: ')
-if zipcode.replace('-', '') and len(zipcode) < 9:
-    url_base = f"https://viacep.com.br/ws/{zipcode}/json/"
-    resq = requests.get(url_base)
-    if resq.status_code == 200:
-        d = resq.json()
-    else:
-        print("Erro")
-else:
-    print("CEP inválido")
-
 
 def get_info_logradouro():
     return d['logradouro']
@@ -52,13 +41,25 @@ def get_info_cep():
     return d['cep']
 
 
-print(f"CEP: {get_info_cep()}")
-print(f"Logradouro: {get_info_logradouro()}")
-print(f"Complemento: {get_info_complemento()}")
-print(f"Bairro: {get_info_bairro()}")
-print(f"Cidade: {get_info_city()}")
-print(f"UF: {get_info_uf()}")
-print(f"DDD da Cidade: {get_info_ddd()}")
-print(f"IBGE: {get_info_ibge()}")
-print(f"GIA: {get_info_gia()}")
-print(f"Siafi: {get_info_siafi()}")
+zipcode = input('Digite o CEP: ')
+code = zipcode.replace('-', '')
+
+if len(code) < 9:
+    url_base = f"https://viacep.com.br/ws/{code}/json/"
+    resq = requests.get(url_base)
+    if resq.status_code == 200:
+        d = resq.json()
+        print(f"CEP: {get_info_cep()}")
+        print(f"Logradouro: {get_info_logradouro()}")
+        print(f"Complemento: {get_info_complemento()}")
+        print(f"Bairro: {get_info_bairro()}")
+        print(f"Cidade: {get_info_city()}")
+        print(f"UF: {get_info_uf()}")
+        print(f"DDD da Cidade: {get_info_ddd()}")
+        print(f"IBGE: {get_info_ibge()}")
+        print(f"GIA: {get_info_gia()}")
+        print(f"Siafi: {get_info_siafi()}")
+    else:
+        print("Erro")
+else:
+    print("CEP inválido, digite novamente um CEP válido!")
